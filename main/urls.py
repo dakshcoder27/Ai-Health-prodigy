f71d6b6997e54f735bf4a913e53376466d23477e
Daksh Billa <dakshbilla2004@gmail.com>
2025-05-04 14:55:06 +0530

    removed the not required paths

--- main/urls.py
+++ main/urls.py
@@ -9,23 +9,9 @@ urlpatterns = [
 
     path('alzheimer/' , views.alzheimerPred , name = 'alzheimer-predict'),
 
-    path('cancer/' , views.cancer , name = 'cancer-view'),
-    path('cancer-prediction/' , views.cancerPred , name = 'cancer-predict'),
-
     path('brain-tumor/' , views.brainPred , name = 'brain-tumor-predict'),
-
-    path('covid/' , views.covidPred , name = 'covid-predict'),
-
-    path('diabetes/' , views.diabetes , name = 'diabetes-view'),
-    path('diabetes-prediction/' , views.diabetesPred , name = 'diabetes-predict'),
-
-    path('glaucoma/' , views.glaucomaPred , name = 'glaucoma-predict'),
-
-    path('heart-disease/' , views.heart , name = 'heart-disease-view'),
-    path('heart-disease-prediction/' , views.heartPred , name = 'heart-disease-predict'),
 
     path('liver/' , views.liver , name = 'liver-view'),
     path('liver-prediction/' , views.liverPred , name = 'liver-predict'),
 
-    path('malaria/' , views.malariaPred , name = 'malaria-predict'),
 ]
